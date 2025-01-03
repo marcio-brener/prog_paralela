f5d3c404c6110baf95b7add23e41b756b7f2b9f8
William Spada <williamrspada02@gmail.com>
2025-01-03 20:28:17 -0300

    feat: add MPI parallel

--- find_biggest_repeatable_ingredients.c
+++ find_biggest_repeatable_ingredients.c
@@ -1,9 +1,18 @@
 #include "./helpers/ingredients.h"
 
-int main() {
+int main(int argc, char *argv[]) {
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
     FILE *file = fopen("test_dataset02.csv", "r");
     if (file == NULL) {
-        printf("Erro ao abrir o arquivo!\n");
+        if (rank == 0) {
+            printf("Erro ao abrir o arquivo!\n");
+        }
+        MPI_Finalize();
         return 1;
     }
 
@@ -11,15 +20,37 @@ int main() {
     IngredientCount **ingredients = malloc(0);
     int ingredients_size = 0;
 
+    // Distribute lines to each process
+    int line_count = 0;
     while (fgets(line, sizeof(line), file)) {
-        extract_ingredients(line, &ingredients, &ingredients_size);
+        if (line_count % size == rank) { // Only process lines assigned to this rank
+            extract_ingredients(line, &ingredients, &ingredients_size);
+        }
+        line_count++;
     }
 
     fclose(file);
-    sort_ingredients(ingredients, ingredients_size);
 
-    printf("\nIngredientes após a ordenação:\n");
-    print_biggest_three_ingredients(ingredients);
+    // Gather all ingredient counts
+    int global_ingredients_size;
+    MPI_Allreduce(&ingredients_size, &global_ingredients_size, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+
+    IngredientCount **global_ingredients = malloc(global_ingredients_size * sizeof(IngredientCount *));
+    MPI_Gather(ingredients, ingredients_size, MPI_BYTE, global_ingredients, ingredients_size, MPI_BYTE, 0, MPI_COMM_WORLD);
+
+    // Master process sorts and prints results
+    if (rank == 0) {
+        sort_ingredients(global_ingredients, global_ingredients_size);
+
+        printf("\nIngredientes após a ordenação:\n");
+        print_biggest_three_ingredients(global_ingredients);
+
+        for (int i = 0; i < global_ingredients_size; i++) {
+            free(global_ingredients[i]->value);
+            free(global_ingredients[i]);
+        }
+        free(global_ingredients);
+    }
 
     for (int i = 0; i < ingredients_size; i++) {
         free(ingredients[i]->value);
@@ -27,5 +58,6 @@ int main() {
     }
     free(ingredients);
 
+    MPI_Finalize();
     return 0;
 }
